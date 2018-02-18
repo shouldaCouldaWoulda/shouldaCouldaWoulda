598ae9a231c713545170a57167b5d8e155ee302b
Viji <aathavan.bagawan@gmail.com>
2018-02-18 13:30:43 -0500

    js mostly complete, Api call having one issue

--- scripts/templateScript.js
+++ scripts/templateScript.js
@@ -3,6 +3,8 @@ let bitApp = {};
 bitApp.startEpoch = 0;
 bitApp.endEpoch = 0;
 bitApp.epochGraphIncrement = 0;
+bitApp.BTCBuyPrice = 0;
+bitApp.BTCSellPrice = 0;
 
 Date.prototype.getUnixTime = function () { return this.getTime() / 1000 | 0 };
 if (!Date.now) Date.now = function () { return new Date(); }
@@ -10,26 +12,41 @@ if (!Date.now) Date.now = function () { return new Date(); }
 Date.time = function () { return Date.now().getUnixTime(); }
 
 bitApp.epochGraph = function (end, start) {
-    return Math.floor((end - start) / 11);
+    // return Math.floor((end - start) / 10);
+    return (end - start) / 10;
 };
 
-const dates = [];
+// $.when(...BTCPricesHistory)
+//     .then((...args) => {
+//         args = args.map(data => data[0]);
+//     });
 
-bitApp.BTCLoop = function() { for (let i = bitApp.startEpoch; i <= bitApp.endEpoch; i=i+epochGraphIncrement) {
-    dates.push(bitApp.buyBTC(i));
-}
+let numberofEpochPoints = [1, 2, 3, 4, 5, 6, 7, 8]
+const epochDates = [];
+bitApp.EpochArray = function () {
+ for (let i = 0; i < numberofEpochPoints.length; i++){
+    epochDates.push(bitApp.startEpoch + (bitApp.epochGraphIncrement * numberofEpochPoints[i]));
+ }
 }
 
+const BTCPricesHistory = [];
+bitApp.BTCLoop = function() { 
+    for (let i = 0; i < epochDates.length; i++) {
+        BTCPricesHistory.push(bitApp.buyBTC(epochDates[i]));
+        console.log(BTCPricesHistory);
+    }
+}
 bitApp.buyBTC = (date) => {
     let url = `https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=USD,EUR&ts=${date}`
+    
     $.ajax({
-        url: url,
-        method: 'GET',
-        dataType: 'json'
-    }).then(function (res) {
-        console.log(res);
-        bitApp.buyBTCValue = res.BTC.USD;
-        console.log(bitApp.buyBTCValue);
+            url: url,
+            method: 'GET',
+            dataType: 'json'
+        }).then(function (res) {
+        // console.log(res);
+        console.log(res.BTC); 
+        // console.log(bitApp.buyBTCValue);
     });
 };
 
@@ -49,9 +66,18 @@ bitApp.events = function () {
         bitApp.endEpoch = await sellDateEpochValue;
 
         bitApp.epochGraphIncrement = await bitApp.epochGraph (sellDateEpochValue, buyDateEpochValue);
-        console.log(bitApp.epochGraphIncrement);
+
+        await bitApp.EpochArray();
 
         bitApp.BTCLoop();
+
+        bitApp.BTCBuyPrice = bitApp.buyBTC(bitApp.startEpoch);
+        console.log('buyprie is' + bitApp.BTCPrice);
+
+        bitApp.BTCSellPrice = bitApp.buyBTC(bitApp.endEpoch);
+        console.log('sellprice is' + bitApp.BTCSellPrice);
+
+        // bitApp.epochDatesLoop();
 
         
 
